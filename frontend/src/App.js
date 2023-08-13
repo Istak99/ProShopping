bb62d26e541ce766abd70e4ce3e69db31e2fceb9
Istak Ahmed <istakahmedjim@yahoo.com>
2023-08-13 20:21:00 +0600

    ProShopping

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,23 @@
-function App() {
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {Container} from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+const App= ()=> {
   return (
-    <>
-      <h1>Welcome to ProShopping</h1>
-    </>
+    <Router>
+      <Header/>
+      <main className="py-3">
+        <Container>
+          <Routes>
+            <Route path="/" element={<HomeScreen/>} exact/>
+            <Route path="/product/:id" element={<ProductScreen/>}/>
+          </Routes>
+        </Container>
+      </main>
+      <Footer/>
+    </Router>
   );
 }
 
