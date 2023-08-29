a63e0b0373444c7d6cb925aa3cab0da33f0c82f8
Istak Ahmed <istakahmedjim@yahoo.com>
2023-08-30 05:15:17 +0600

    Bringing Redux State into HomeScreen

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -21,8 +21,7 @@ export const listProducts = () => async(dispatch) =>  {
         dispatch({
             type: PRODUCT_LIST_FAIL,
             payload: error.response && error.response.data.message ? error.response.data.message : error.message,
-            
+
         })
     }
-
 }
