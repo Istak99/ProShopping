7a90eafadb2465a37d7cf8600d24006f1102f536
Istak Ahmed <istakahmedjim@yahoo.com>
2023-09-06 07:27:32 +0600

    Show User In Navbar _ Logout

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,32 +1,52 @@
 import React from 'react'
+import {useDispatch, useSelector} from "react-redux"
 import {LinkContainer} from 'react-router-bootstrap'
-import {Container, Nav, Navbar} from "react-bootstrap"
+import {Container, Nav, NavDropdown, Navbar} from "react-bootstrap"
+import { logout } from '../actions/userActions'
+
+
 const Header = () => {
-  return (
-    <header>
-        <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
-            <Container>
-                <LinkContainer to="/">
-                    <Navbar.Brand>ProShopping</Navbar.Brand>
-                </LinkContainer>
-                
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ms-auto">
+    
+    const dispatch=useDispatch( )
+    const userLogin = useSelector(state => state.userLogin)
+    const {userInfo} = userLogin
 
-                        <LinkContainer to="/cart">
-                            <Nav.Link ><i className='fas fa-shopping-cart'></i> Cart</Nav.Link>
-                        </LinkContainer>
+    const logoutHandler=()=> {
+        dispatch(logout())
+    }
 
-                        <LinkContainer to="/login">
-                            <Nav.Link><i className='fas fa-user'></i> Sign In</Nav.Link>  
-                        </LinkContainer>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    </header>
-  )
+    return (
+        <header>
+            <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
+                <Container>
+                    <LinkContainer to="/">
+                        <Navbar.Brand>ProShopping</Navbar.Brand>
+                    </LinkContainer>
+                    
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ms-auto">
+
+                            <LinkContainer to="/cart">
+                                <Nav.Link ><i className='fas fa-shopping-cart'></i> Cart</Nav.Link>
+                            </LinkContainer>
+                            {userInfo ? ( 
+                               <NavDropdown title={userInfo.name} id='username'>
+                                    <LinkContainer to='/profile'>
+                                        <NavDropdown.Item>Profile</NavDropdown.Item>
+                                    </LinkContainer>
+                                    <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
+                               </NavDropdown>
+                            ) : <LinkContainer to="/login">
+                                    <Nav.Link><i className='fas fa-user'></i> Sign In</Nav.Link>  
+                                </LinkContainer>
+                            }
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
+    )
 }
 
 export default Header
