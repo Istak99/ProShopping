9a807a3d7363301eec0846806a97b25a84e994e1
Istak Ahmed <istakahmedjim@yahoo.com>
2023-09-12 02:09:34 +0600

    Update User Profile

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -18,7 +18,7 @@ import {combineReducers, configureStore} from '@reduxjs/toolkit'
 import thunk from 'redux-thunk'
 import {productListReducer, productDetailsReducer} from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
-import { userLoginReducer, userRegisterReducer, userDetailsReducer } from './reducers/userReducers'
+import { userLoginReducer, userRegisterReducer, userDetailsReducer, userUpdateProfileReducer } from './reducers/userReducers'
 
 
 
@@ -29,6 +29,7 @@ const reducer = combineReducers({
   userLogin: userLoginReducer,
   userRegister: userRegisterReducer,
   userDetails: userDetailsReducer,
+  userUpdateProfile: userUpdateProfileReducer,
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(
